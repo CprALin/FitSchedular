8a4f0488c9d26d8c513fe4223969182965880405
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-15 12:12:18 +0200

    base model for appointment

---
+++ models/GymAppointmentModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Participation = require('./ParticipationModel');
+
+const appointmentSchema = new mongoose.Schema({
+     trainer : {
+        type : mongoose.Schema.ObjectId,
+        ref : 'Trainer',
+        required : [true, 'An appointment must belog to a trainer !']
+     },
+     onDate : {
+        type : Date,
+        required : [true, 'An appointment must have a date !']
+     },
+     startHour : {
+        type : Number,
+        require : [true, 'An appointment must have a start hour !']
+     },
+     finishHour : {
+        type : Number,
+        require : [true, 'An appointment must have a finish hour !']
+     },
+     participations : {
+        type : mongoose.Schema.ObjectId,
+        ref : 'Participation'
+     }
+});
+
+const Appointment = mongoose.model('Appointment' , appointmentSchema);
+
+module.exports = Appointment;
