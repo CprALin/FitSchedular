9df49f446ae1e8c9c95eeaf0b09ec0e307b28578
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-15 12:13:12 +0200

    base model for trainer

---
+++ models/TrainerModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const trainerSchema = new mongoose.Schema({
+    user : {
+        type : mongoose.Schema.ObjectId,
+        ref : 'User',
+        required : [true, 'Trainer must be a User !']
+    },
+    className : {
+        type : String,
+        required : [true, 'Trainer must have a class !']
+    },
+    classDescription : {
+        type : String,
+        required : [true, 'A class must have a description !']
+    },
+    occupation : {
+        type : String,
+        required : [true , 'A trainer must have an occupation !']
+    },
+    studies : {
+        type : String,
+        required : [true, 'A trainer must have some studies !']
+    }
+});
+
+const Trainer = mongoose.model('Trainer' , trainerSchema);
+
+module.exports = Trainer;
