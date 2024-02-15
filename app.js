9dbf1d9a3549cf9f51755d7b1d6ca0565ca4e402
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-15 17:53:33 +0200

    -

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 
+const AppointmentRouter = require('./routes/AppointmentRoutes');
+const ParticipationRouter = require('./routes/ParticipationRoutes');
+const ReviewRouter = require('./routes/ReviewRoutes');
+const TrainerRouter = require('./routes/TrainerRoutes');
+const UserRouter = require('./routes/UserRoutes');
+
 const app = express();
 
+app.use('/api/reviews' , ReviewRouter);
+app.use('/api/appointments' , AppointmentRouter);
+app.use('/api/participations' , ParticipationRouter);
+app.use('/api/trainers' , TrainerRouter);
+app.use('/api/users' , UserRouter);
 
 module.exports = app;
