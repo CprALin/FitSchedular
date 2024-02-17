dab98397cf8a2e26e420eb5ac73d7965b7221636
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-17 19:23:28 +0200

    -

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,40 @@ const User = require('../models/UserModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./HandlerFactory');
+const multer = require('multer');
+const sharp = require('sharp');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req , file , cb) => {
+    if(file.mimetype.startsWith('image')){
+        cb(null,true)
+    }else{
+        cb(new AppError('Not an image! Please upload only images.', 400), false);
+    }
+};
+
+//upload photo
+const upload = multer({
+    storage : multerStorage,
+    fileFilter : multerFilter
+});
+
+exports.uploadUserPhoto = upload.single('photo'); 
+
+exports.resizeUserPhoto = catchAsync(async (req , res , next) => {
+    if(!req.file) return next();
+
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+    await sharp(req.file.buffer)
+        .resize(500,500, sharp.fit)
+        .toFormat('jpeg')
+        .jpeg({quality : 90})
+        .toFile(`public/img/users/${req.file.filename}`);
+
+    next();
+});
 
 // save just allowedFields 
 const filterObj = (obj , ...allowedFields) => {
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UserRouter = require('../controllers/UserController');
 const authController = require('../controllers/AuthController');
 
+
 //ROUTES
 const router = express.Router();    
 
@@ -10,6 +11,22 @@ router.post('/singup' , authController.singup);
 router.post('/login' , authController.login);
 
 router.get('/logout' , authController.logout);
+
+router.post('/forgotPassword' , authController.forgotPassword);
+
+router.patch('/resetPassword/:token' , authController.resetPassword);
+
+router.use(authController.protect);
+
+router.patch('/updateMyPassword' , authController.updatePassword);
+
+router.get('/me' , UserRouter.getMe , UserRouter.getUser);
+
+router.delete('/deleteMe' , UserRouter.deleteMe);
+
+router.use(authController.restrictTo('admin'));
+
+router.patch('/updateMe' , UserRouter.uploadUserPhoto, UserRouter.resizeUserPhoto , UserRouter.updateMe);
 
 router.route('/')
       .get(UserRouter.getAllUsers)
