203cd4347381017fbac2d323f563db47c7374848
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-25 20:18:40 +0200

    -

--- src/Components/Body/StartPage/ShortTrainers.js
+++ src/Components/Body/StartPage/ShortTrainers.js
@@ -1,7 +1,27 @@
+import image1 from "../../../Images/trainer1.jpg";
+import image2 from "../../../Images/trainer2.jpg";
+import image3 from "../../../Images/trainer3.jpg";
+
 function ShortTrainers(){
     return(
         <div className="short-trainers">
-            <p>Trainers</p>
+            <h1>Team of professional trainers</h1>
+
+            <div className="trainer-containers">
+
+                <div className="trainer-container">
+                    <img src={image1} alt="trainer" />
+                </div>
+
+                <div className="trainer-container">
+                    <img src={image2} alt="trainer" />
+                </div>
+
+                <div className="trainer-container">
+                    <img src={image3} alt="trainer" />
+                </div>
+
+            </div>
         </div>
     );
 }
