d48f31ff324006f0ec3f1e203209aab4c4a5e5fb
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-02-26 10:44:00 +0200

    -

--- src/Components/Body/StartPage/Subscriptions.js
+++ src/Components/Body/StartPage/Subscriptions.js
@@ -1,7 +1,87 @@
+import Button from "../../ReuseComp/Button";
+import { FaCheck } from "react-icons/fa6";
+
 function Subscriptions(){
     return(
-        <div className="subscriptions scroll-target" id="subs">
-            <p>Subscriptions</p>
+        <div className="subscriptions" id="subs">
+           
+            <div className="subscription-intro">
+                <h1 className="center">Packages</h1>
+                <div className="sub-p">
+                    <p>
+                        Explore your fitness possibilities. 
+                        Take a look at our packages designed
+                        to meet your needs and start your journey.
+                    </p>
+                </div>
+            </div>
+
+            <div className="subscription-containers">
+
+                <div className="subscription-container">
+                     <h1>$60</h1>
+                     <section>
+                        <p>Starter Package</p>
+                        <p>Billed annually</p>
+                     </section>
+
+                     <section>
+                        <p><span><FaCheck /></span>Unlimited access to gym</p>
+                        <p><span><FaCheck /></span>Facilities during regular hours</p>
+                        <p><span><FaCheck /></span>Group fitness classes twice a week</p>
+                        <p><span><FaCheck /></span>Access to general workout plans</p>
+                        <p><span><FaCheck /></span>24/7 gym access</p>
+                     </section>
+                    
+
+                     <div className="subscription-btn">
+                         <Button padding={'10px 60px'}>Get started</Button>
+                     </div>
+                </div>
+
+                <div className="subscription-container">
+                     <h1>$84</h1>
+                     <section>
+                        <p>Starter Package</p>
+                        <p>Billed annually</p>
+                     </section>
+
+                     <section>
+                        <p><span><FaCheck /></span>Unlimited access to gym</p>
+                        <p><span><FaCheck /></span>Facilities during regular hours</p>
+                        <p><span><FaCheck /></span>Group fitness classes twice a week</p>
+                        <p><span><FaCheck /></span>Access to general workout plans</p>
+                        <p><span><FaCheck /></span>24/7 gym access</p>
+                     </section>
+                    
+
+                     <div className="subscription-btn">
+                         <Button padding={'10px 60px'}>Get started</Button>
+                     </div>
+                </div>
+
+                <div className="subscription-container">
+                     <h1>$120</h1>
+                     <section>
+                        <p>Starter Package</p>
+                        <p>Billed annually</p>
+                     </section>
+
+                     <section>
+                        <p><span><FaCheck /></span>Unlimited access to gym</p>
+                        <p><span><FaCheck /></span>Facilities during regular hours</p>
+                        <p><span><FaCheck /></span>Group fitness classes twice a week</p>
+                        <p><span><FaCheck /></span>Access to general workout plans</p>
+                        <p><span><FaCheck /></span>24/7 gym access</p>
+                     </section>
+                    
+
+                     <div className="subscription-btn">
+                         <Button padding={'10px 60px'}>Get started</Button>
+                     </div>
+                </div>
+
+            </div>
         </div>
     );
 }   
