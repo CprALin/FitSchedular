0185d1d684ee4cf19f8315bc2f57da70ae1fe6e2
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-03-12 09:38:45 +0200

    -

--- src/Components/Body/LoginPage/LoginPage.js
+++ src/Components/Body/LoginPage/LoginPage.js
@@ -1,5 +1,7 @@
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import Button from '../../ReuseComp/Button';
+import { Link } from 'react-router-dom';
 
 function LoginPage(){
 
@@ -8,13 +10,17 @@ function LoginPage(){
     return(
         <div className="login-page">
             <div className="login-form">
-                <h1>Login</h1>
+                <h1>Hello !</h1>
                 <FloatingLabel controlId="floatingInput" label="Email Adress" className="mb-3">
                     <Form.Control type="email" placeholder="name@example.com" />
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
                     <Form.Control type="password" placeholder="Password" />
-                </FloatingLabel>  
+                </FloatingLabel>
+                <div className='btn-register'>
+                   <Button>Login</Button>  
+                   <p>You don't have an account ? <Link>Register</Link></p>
+                </div>
             </div>
         </div>
        
