830cf9ae99f5b16e4f8ae892f5db43d7b82565f7
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-03-12 13:54:27 +0200

    -

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 
 const AppointmentRouter = require('./routes/AppointmentRoutes');
@@ -47,6 +48,7 @@ app.use((req , res , next) => {
     next();
 });
 
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
