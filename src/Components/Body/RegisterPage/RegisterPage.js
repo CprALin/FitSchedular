8d5c83cc1feb9ee63edea06904ac689ade5f85a6
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-03-12 09:52:34 +0200

    -

--- src/Components/Body/RegisterPage/RegisterPage.js
+++ src/Components/Body/RegisterPage/RegisterPage.js
@@ -3,28 +3,28 @@ import Form from 'react-bootstrap/Form';
 import Button from '../../ReuseComp/Button';
 import { Link } from 'react-router-dom';
 
-function LoginPage(){
-
-    window.scrollTo({ top : 0});
-
+function RegisterPage(){
     return(
-        <div className="login-page">
-            <div className="login-form">
-                <h1>Hello !</h1>
+        <div className="register-page">
+            <div className="register-form">
+                <h2>Hello !</h2>
+                <FloatingLabel controlId="floatingInputName" label="Name" className="mb-3">
+                    <Form.Control type="text" placeholder="Name" />
+                </FloatingLabel>
                 <FloatingLabel controlId="floatingInputEmail" label="Email Adress" className="mb-3">
                     <Form.Control type="email" placeholder="name@example.com" />
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
                     <Form.Control type="password" placeholder="Password" />
                 </FloatingLabel>
+                <FloatingLabel controlId="floatingConfirmPassword" label="Confirm Password" className="mb-3">
+                    <Form.Control type="password" placeholder="Password" />
+                </FloatingLabel>
                 <div className='btn-register'>
-                   <Button>Login</Button>  
-                   <p>You don't have an account ? <Link>Register</Link></p>
+                   <Button>Register</Button>  
+                   <p>You have an account ? <Link>Login</Link></p>
                 </div>
             </div>
         </div>
-       
     );
 }
-
-export default LoginPage;
