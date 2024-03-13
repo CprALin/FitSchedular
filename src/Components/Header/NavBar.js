e7be9d98b6d4ff786366ed38b749a71444866db7
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-03-13 16:50:10 +0200

    -

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -21,11 +21,11 @@ function NavBar(){
             <Logo />
             <NavList />
             {isOnLoginPage ? null : (user ? 
-               <>
+               <div className="nav-user-div">
                     <UserInfo /> 
-                    <Button event={logout}>Logout</Button> 
-               </>
-             : <Button event={handlePageClick}>Login</Button>) }
+                    <Button event={logout} padding={'2.5px 24px'}>Logout</Button> 
+               </div>
+             : <Button event={handlePageClick} padding={'10px 29px'}>Login</Button>) }
         </div>
     );
 }
